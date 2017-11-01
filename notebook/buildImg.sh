7880ada13c681cc793ce420df65c17a5315b039d
Jirka Kremser <jkremser@redhat.com>
2017-11-01 17:27:28 +0100

    update to notebooks

--- notebook/buildImg.sh
+++ notebook/buildImg.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 GRAPH_DATA1=/home/jkremser/bitcoin/output-0051-0054 \
 GRAPH_DATA2=/home/jkremser/bitcoin/output-0401-0404 \
-GRAPH_DATA3=/home/jkremser/bitcoin/output-0801-0804 make build
+make build
