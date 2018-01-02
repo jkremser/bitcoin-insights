be7cda2e50631eb03cfd979a29801ce93491c8c8
Jirka Kremser <jkremser@redhat.com>
2018-01-02 17:10:05 +0100

    Adding types to the parquet-converter

--- parquet-converter/src/main/scala/io/radanalytics/bitcoin/ParquetConverter.scala
+++ parquet-converter/src/main/scala/io/radanalytics/bitcoin/ParquetConverter.scala
@@ -26,6 +26,8 @@ import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 import org.apache.hadoop.conf._
 import org.apache.spark.graphx._
+import org.apache.spark.graphx.Edge
+import org.apache.spark.rdd.RDD
 import org.apache.hadoop.io._
 import org.apache.spark.sql.SparkSession
 import org.zuinnote.hadoop.bitcoin.format.common._
@@ -33,6 +35,16 @@ import org.zuinnote.hadoop.bitcoin.format.mapreduce._
 
 object ParquetConverter {
   var debug = 0;
+
+  type TXHashBytes = Array[Byte]
+  type TXHashByteArray = ByteArray
+  type TXIoIndex = Long
+  type TX_ID = (TXHashByteArray, TXIoIndex)
+  type Input = TX_ID
+  type Output = TX_ID
+  type BtcAddress = String
+  type TxData = (BtcAddress, TXHashBytes, TXIoIndex, TXHashBytes, TXIoIndex)
+  type VertexId = Long
 
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("Bitcoin insights - ParquetConverter)")
@@ -44,12 +56,12 @@ object ParquetConverter {
   }
 
   def convert(sc: SparkContext, hadoopConf: Configuration, inputFile: String, outputDir: String) = {
-    if (debug == 1) println("\n\n\n\n\n                          ********************invoking convert()****************              \n\n\n\n\n\n\n\n")
+    if (debug >= 1) println("\n\n\n\n\n                          ********************invoking convert()****************              \n\n\n\n\n\n\n\n")
     val bitcoinBlocksRDD = sc.newAPIHadoopFile(inputFile, classOf[BitcoinBlockFileInputFormat], classOf[BytesWritable], classOf[BitcoinBlock], hadoopConf)
 
     // extract a tuple per transaction containing Bitcoin destination address, the input transaction hash,
     // the input transaction output index, and the current transaction hash, the current transaction output index, a (generated) long identifier
-    val bitcoinTransactionTuples = bitcoinBlocksRDD.flatMap(hadoopKeyValueTuple => extractTransactionData(hadoopKeyValueTuple._2))
+    val bitcoinTransactionTuples: RDD[TxData] = bitcoinBlocksRDD.flatMap(hadoopKeyValueTuple => extractTransactionData(hadoopKeyValueTuple._2))
 //    bitcoinTransactionTuples.cache()
 
 //    // RDD[(BytesWritable, BitcoinBlock)]
@@ -58,37 +70,37 @@ object ParquetConverter {
 //    foo.foreach(hadoopKeyValueTuple => extractTransactionData(hadoopKeyValueTuple._2))
 //    println(foo)
 
-    // create the vertex (vertexId, Bitcoin destination address), keep in mind that the flat table contains the same bitcoin address several times
-    val bitcoinAddressIndexed = bitcoinTransactionTuples.map(bitcoinTransactions => bitcoinTransactions._1).distinct().zipWithIndex()
+    // create the vertex (Bitcoin destination address, vertexId), keep in mind that the flat table contains the same bitcoin address several times
+    val bitcoinAddressIndexed: RDD[(BtcAddress, VertexId)] = bitcoinTransactionTuples.map(bitcoinTransactions => bitcoinTransactions._1).distinct().zipWithIndex()
 //    bitcoinAddressIndexed.cache()
 
     // create the edges (bitcoinAddress,(byteArrayTransaction, TransactionIndex)
-    val inputTransactionTuple = bitcoinTransactionTuples.map(bitcoinTransactions =>
+    val inputTransactionTuple: RDD[(BtcAddress, Input)] = bitcoinTransactionTuples.map(bitcoinTransactions =>
       (bitcoinTransactions._1, (new ByteArray(bitcoinTransactions._2), bitcoinTransactions._3)))
 
     // (bitcoinAddress,((byteArrayTransaction, TransactionIndex),vertexId))
-    val inputTransactionTupleWithIndex = inputTransactionTuple.join(bitcoinAddressIndexed)
+    val inputTransactionTupleWithIndex: RDD[(BtcAddress, (Input, VertexId))] = inputTransactionTuple.join(bitcoinAddressIndexed)
 
     // (byteArrayTransaction, TransactionIndex), (vertexId, bitcoinAddress)
-    val inputTransactionTupleByHashIdx = inputTransactionTupleWithIndex.map(iTTuple => (iTTuple._2._1, (iTTuple._2._2, iTTuple._1)))
+    val inputTransactionTupleByHashIdx: RDD[(Input, (VertexId, BtcAddress))] = inputTransactionTupleWithIndex.map(iTTuple => (iTTuple._2._1, (iTTuple._2._2, iTTuple._1)))
 
-    val currentTransactionTuple = bitcoinTransactionTuples.map(bitcoinTransactions =>
+    val currentTransactionTuple: RDD[(BtcAddress, Output)] = bitcoinTransactionTuples.map(bitcoinTransactions =>
       (bitcoinTransactions._1, (new ByteArray(bitcoinTransactions._4), bitcoinTransactions._5)))
-    val currentTransactionTupleWithIndex = currentTransactionTuple.join(bitcoinAddressIndexed)
+    val currentTransactionTupleWithIndex: RDD[(BtcAddress, (Output, VertexId))] = currentTransactionTuple.join(bitcoinAddressIndexed)
 
     // (byteArrayTransaction, TransactionIndex), (vertexId, bitcoinAddress)
-    val currentTransactionTupleByHashIdx = currentTransactionTupleWithIndex.map { cTTuple => (cTTuple._2._1, (cTTuple._2._2, cTTuple._1)) }
+    val currentTransactionTupleByHashIdx: RDD[(Output, (VertexId, BtcAddress))] = currentTransactionTupleWithIndex.map { cTTuple => (cTTuple._2._1, (cTTuple._2._2, cTTuple._1)) }
 
     // the join creates ((ByteArray, Idx), (srcIdx,srcAddress), (destIdx,destAddress)
-    val joinedTransactions = inputTransactionTupleByHashIdx.join(currentTransactionTupleByHashIdx)
+    val joinedTransactions: RDD[(TX_ID, ((VertexId, BtcAddress), (VertexId, BtcAddress)))] = inputTransactionTupleByHashIdx.join(currentTransactionTupleByHashIdx)
 
     // create vertices => vertexId,bitcoinAddress
-    val bitcoinTransactionVertices = bitcoinAddressIndexed.map { case (k, v) => (v, k) }
+    val bitcoinTransactionVertices: RDD[(VertexId, BtcAddress)] = bitcoinAddressIndexed.map { case (k, v) => (v, k) }
 
     // create edges
-    val bitcoinTransactionEdges = joinedTransactions.map(joinTuple => Edge(joinTuple._2._1._1, joinTuple._2._2._1, "input"))
+    val bitcoinTransactionEdges: RDD[Edge[String]] = joinedTransactions.map(joinTuple => Edge(joinTuple._2._1._1, joinTuple._2._2._1, "input"))
 
-    if (debug == 1) println("\n\n\n\n\n                          ********************saving parquet files****************              \n\n\n\n\n\n\n\n")
+    if (debug >= 1) println("\n\n\n\n\n                          ********************saving parquet files****************              \n\n\n\n\n\n\n\n")
 
     // create two parquet files, one with nodes and second with edges
     val spark = SparkSession
@@ -100,8 +112,8 @@ object ParquetConverter {
   }
 
   // extract relevant data
-  def extractTransactionData(bitcoinBlock: BitcoinBlock): Array[(String, Array[Byte], Long, Array[Byte], Long)] = {
-    if (debug == 1) println("\n\n\n\n\n                          ********************invoking extractTransactionData()****************              \n\n\n\n\n\n\n\n")
+  def extractTransactionData(bitcoinBlock: BitcoinBlock): Array[TxData] = {
+    if (debug >= 1) println("\n\n\n\n\n                          ********************invoking extractTransactionData()****************              \n\n\n\n\n\n\n\n")
 
     // first we need to determine the size of the result set by calculating the total number of inputs
     // multiplied by the outputs of each transaction in the block
@@ -113,7 +125,7 @@ object ParquetConverter {
 
     // then we can create a tuple for each transaction input: Destination Address (which can be found in the output!), Input Transaction Hash, Current Transaction Hash, Current Transaction Output
     // as you can see there is no 1:1 or 1:n mapping from input to output in the Bitcoin blockchain, but n:m (all inputs are assigned to all outputs), cf. https://en.bitcoin.it/wiki/From_address
-    val result: Array[(String, Array[Byte], Long, Array[Byte], Long)] = new Array[(String, Array[Byte], Long, Array[Byte], Long)](resultSize)
+    val result: Array[TxData] = new Array[TxData](resultSize)
     var resultCounter: Int = 0
     for (i <- 0 to transactionCount - 1) { // for each transaction
       val currentTransaction = bitcoinBlock.getTransactions().get(i)
@@ -130,7 +142,7 @@ object ParquetConverter {
 
           // example of 'normal' tx: https://blockchain.info/tx/fb308839d7410a9b5ee9f4a7a36ab38908219d14141b607965640edf727445d1
           // i.e. 1 sender, 1 receiver and sending back the rest
-          if (debug == 2) {
+          if (debug >= 2) {
             println("\n\n\n\n\n                          ********************TX****************              \n\n\n\n\n\n\n\n")
             println("currentTransactionInputHash = " + currentTransactionInputHash)
             println("currentTransactionHash = " + currentTransactionHash)
