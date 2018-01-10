f03570892ad4a94880c41c83cd30cb07c4309db5
Jirka Kremser <jkremser@redhat.com>
2018-01-10 14:49:54 +0100

    make test passing, makefile foo

--- parquet-converter/src/test/scala/io/radanalytics/bitcoin/TransactionsSpec.scala
+++ parquet-converter/src/test/scala/io/radanalytics/bitcoin/TransactionsSpec.scala
@@ -5,10 +5,10 @@
 */
 package io.radanalytics.bitcoin
 
-import java.io.{FileOutputStream, ObjectOutputStream}
 
 import org.scalatest.{BeforeAndAfterAll, FlatSpec, GivenWhenThen, Matchers}
-import org.zuinnote.hadoop.bitcoin.format.common.{BitcoinScriptPatternParser, BitcoinTransactionInput, BitcoinUtil}
+import org.zuinnote.hadoop.bitcoin.format.common.{BitcoinTransactionInput, BitcoinUtil}
+
 
 
 class TransactionsSpec extends FlatSpec with BeforeAndAfterAll with GivenWhenThen with Matchers  {
@@ -27,24 +27,17 @@ override def beforeAll(): Unit = {
 
 
 
-"Simple Test" should "be always successful" in {
+"Hash functions and converting them to hex" should "be always successful" in {
   import java.io.FileInputStream
   import java.io.ObjectInputStream
-  val ois = new ObjectInputStream(new FileInputStream("/home/jkremser/foob"))
+  val source = getClass.getResource("/serialized").getPath
+  val ois = new ObjectInputStream(new FileInputStream(source))
   val input = ois.readObject.asInstanceOf[BitcoinTransactionInput]
 
-  println("tx: " + input.getPrevTransactionHash)
-
-  println(BitcoinUtil.convertByteArrayToHexString(input.getTxInScript))
-
-//  println("\n\npicinka: " + input.getPrevTransactionHash)
-//  println("\n\nkucinka: " + BitcoinScriptPatternParser.getPaymentDestination(input.getPrevTransactionHash))
-////  println("\n\nnicinka: " + input.getTxInScriptLength)
-////  println("\n\nkacinka: " + BitcoinScriptPatternParser.getPaymentDestination(input.getTxInScriptLength))
-//  println("\n\nkonicinka: " + BitcoinUtil.convertByteArrayToHexString(input.getPrevTransactionHash))
-//  println("\n\nfocinka: " + BitcoinUtil.convertByteArrayToHexString(BitcoinUtil.reverseByteArray(input.getPrevTransactionHash)))
-////  println("\n\npocinka: " + BitcoinUtil.convertByteArrayToHexString(input.getTxInScript))
-////  println("\n\nnecinka: " + BitcoinUtil.convertByteArrayToHexString(BitcoinUtil.reverseByteArray(input.getTxInScript)))
+  assert(BitcoinUtil.convertByteArrayToHexString(input.getPrevTransactionHash) == "9609319FA8102E0506E86E179A142AC4D1B187CD56F508E9F58FC18B0E4DEB11")
+  assert(BitcoinUtil.convertByteArrayToHexString(BitcoinUtil.reverseByteArray(input.getPrevTransactionHash)) == "11EB4D0E8BC18FF5E908F556CD87B1D1C42A149A176EE806052E10A89F310996")
+  assert(BitcoinUtil.convertByteArrayToHexString(input.getTxInScript) == "493046022100829C046C9D820F6290EB3281E510D1D102AB5BBFD0B0F51DBEF37BA845C237F50221009A43D1F81F2A37AAD357EF0A1243AD66AD9C7F4F223554380AF1921CC41B051601")
+  assert(BitcoinUtil.convertByteArrayToHexString(BitcoinUtil.reverseByteArray(input.getTxInScript)) == "0116051BC41C92F10A385435224F7F9CAD66AD43120AEF57D3AA372A1FF8D1439A002102F537C245A87BF3BE1DF5B0D0BF5BAB02D1D110E58132EB90620F829D6C049C82002102463049")
 }
 
 
